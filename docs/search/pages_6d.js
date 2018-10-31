e669b6517eacf5cd49bae0c58f292693fdf46c92
redeboer <r.e.deboer@students.uu.nl>
2018-10-31 10:18:35 +0800

    updated from server (21007)

--- docs/search/pages_6d.js
+++ docs/search/pages_6d.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['main_20page_20_3c_21_2d_2d_20_7b_23mainpage_7d_20_2d_2d_3e',['Main Page &lt;!-- {#mainpage} --&gt;',['../md__besfs_users_deboer_BOSS_Afterburner_README.html',1,'']]]
+  ['main_20page',['Main Page',['../index.html',1,'']]]
 ];
