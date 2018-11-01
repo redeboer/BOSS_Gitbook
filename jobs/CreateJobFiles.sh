56d96771db6d1514f4fccae4b75999799d79b087
redeboer <r.e.deboer@students.uu.nl>
2018-11-01 11:00:26 +0800

    updated from server (30400)

--- jobs/CreateJobFiles.sh
+++ jobs/CreateJobFiles.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -
 # * ===============================================================================
+# *   DESCRIPTION: Batch create jobOption files based on a template
 # *        AUTHOR: Remco de Boer (@IHEP), EMAIL: remco.de.boer@ihep.ac.cn
 # *  ORGANIZATION: IHEP, CAS (Beijing, CHINA)
 # *       CREATED: 22 October 2018
@@ -10,109 +11,120 @@
 
 set -o nounset # Treat unset variables as an error
 
-# * ------- Script parameters ------- *
-nJobs=${1}
-analysis_type="test1" # will be used in file naming
-rand_sim=2000         # random number generator for sim option
-nEventsPerJob=10000   # default number of events per job
-scriptFolder="/ihepbatch/bes/$USER/workarea-7.0.3/TestRelease/TestRelease-00-00-86/run"
-	# rtraw, dst, root, and log files will be written to this folder
-outputFolder="/besfs/users/$USER/output"
-	# rtraw, dst, root, and log files will be written to this folder
-if [ $# -ge 2 ]
-then
-	nEventsPerJob=${2} # optional user input for number of events per job
-fi
-if [ ! -d ${outputFolder} ]
-then
-	echo "ERROR: output folder \"${outputFolder}\" does not exist. Check this script..."
-	exit
-fi
-
-# * ------- Functions ------- *
-function CreateOrEmptyOutputSubDir()
-{
-	if [ ! -d "${scriptFolder}/${1}" ]
-	then
-		mkdir "${scriptFolder}/${1}"
-	else
-		rm -rf "${scriptFolder}/${1}/${1}_${analysis_type}_"*".sh"
-		rm -rf "${scriptFolder}/${1}/${1}_${analysis_type}_"*".sh."*"."*
-		rm -rf "${scriptFolder}/${1}/${1}_${analysis_type}_"*".txt"
+# * ------- Script parameters ------- * #
+	nJobs=${1}
+	analysis_type="rhopi" # will be used in file naming
+	rand_sim=2000         # random number generator for sim option
+	nEventsPerJob=10000   # default number of events per job
+	scriptFolder="/ihepbatch/bes/$USER/workarea-7.0.3/TestRelease/TestRelease-00-00-86/run"
+		# rtraw, dst, root, and log files will be written to this folder
+	outputFolder="/besfs/users/$USER/BOSS_Afterburner/data"
+		# rtraw, dst, root, and log files will be written to this folder
+	if [ $# -ge 2 ]; then # optional user input for number of events per job
+		nEventsPerJob=${2}
 	fi
-}
-function CheckTemplateFile()
-{
-	templateName="${1}"
-	if [ ! -s "${templateName}" ]
-	then
-		echo "Template file \"${templateName}\" does not exist"
+
+# * ------- Check parameters ------- * #
+	if [ ! -d ${scriptFolder} ]; then
+		echo -e "\e[91mERROR: output folder \"${scriptFolder}\" does not exist. Check this script...\e[0m"
 		exit
 	fi
-}
+	if [ ! -d ${outputFolder} ]; then
+		echo -e "\e[91mERROR: output folder \"${outputFolder}\" does not exist. Check this script...\e[0m"
+		exit
+	fi
 
-# * ------- Create directories ------- *
-# * Scripts directory
-CreateOrEmptyOutputSubDir "sim"
-CreateOrEmptyOutputSubDir "rec"
-CreateOrEmptyOutputSubDir "ana"
-CreateOrEmptyOutputSubDir "submit"
-# * Output directory
-CreateOrEmptyOutputSubDir "${outputFolder}/rtraw"
-CreateOrEmptyOutputSubDir "${outputFolder}/dst"
-CreateOrEmptyOutputSubDir "${outputFolder}/dst"
-CreateOrEmptyOutputSubDir "${outputFolder}/log"
+# * ------- Functions ------- * #
+	function CreateOrEmptyOutputSubDir()
+	{
+		if [ ! -d "${1}/${2}" ]; then
+			mkdir "${1}/${2}"
+		else
+			rm -rf "${1}/${2}/${2}_${analysis_type}_"*".sh"       # remove sumbit scripts
+			rm -rf "${1}/${2}/${2}_${analysis_type}_"*".sh."*"."* # remove log files
+			rm -rf "${1}/${2}/${2}_${analysis_type}_"*".txt"      # remove jobOptions
+		fi
+	}
+	function CheckTemplateFile()
+	{
+		templateName="${1}"
+		if [ ! -s "${templateName}" ]
+		then
+			echo "Template file \"${templateName}\" does not exist"
+			exit
+		fi
+	}
 
 
-# * ------- Loop over run numbers ------- *
-for jobNo in $(seq 0 $((${nJobs} - 1)))
-do
 
-	# * Generate the simulation files (sim) *
-	CheckTemplateFile "${scriptFolder}/templates/jobOptions_sim_${analysis_type}.txt"
-	awk '{flag = 1}
-		{sub(/RDF/,"rtraw/sim_'${analysis_type}'_'${jobNo}'.rtraw")}
-		{sub(/SCRIPT_PATH/,"'${scriptFolder}'")}
-		{sub(/OUTPUT_PATH/,"'${outputFolder}'")}
-		{sub(/RAND/,'$(($(date +%s%N) % 1000000000))')}
-		{sub(/DEC/,"dec/'${analysis_type}'.dec")}
-		{sub(/NEVENTS/,'${nEventsPerJob}')}
-		{if(flag == 1) {print $0} else {next} }' \
-	${templateName} > sim/sim_${analysis_type}_${jobNo}.txt
+# * ------- Main function ------- * #
 
-	# * Generate the reconstruction files (rec) *
-	CheckTemplateFile "${scriptFolder}/templates/jobOptions_rec_${analysis_type}.txt"
-	awk '{flag = 1}
-		{sub(/RDF/,"rtraw/sim_'${analysis_type}'_'${jobNo}'.rtraw")}
-		{sub(/DSTFILE/,"dst/rec_'${analysis_type}'_'${jobNo}'.dst")}
-		{sub(/OUTPUT_PATH/,"'${outputFolder}'")}
-		{sub(/RAND/,'$(($(date +%s%N) % 1000000000))')}
-		{sub(/NEVENTS/,'${nEventsPerJob}')}
-		{if(flag == 1) {print $0} else {next} }' \
-	${templateName} > rec/rec_${analysis_type}_${jobNo}.txt
+	# * User input
+	echo "This will create ${nJobs} \"${analysis_type}\" job files with ${nEventsPerJob} events each of them."
+	read -p "To continue, press ENTER, else Ctrl+C ..."
 
-	# * Generate the analyse files (ana) *
-	CheckTemplateFile "${scriptFolder}/templates/jobOptions_ana_${analysis_type}.txt"
-	awk '{flag = 1}
-		{sub(/DSTFILE/,"dst/rec_'${analysis_type}'_'${jobNo}'.dst")}
-		{sub(/ROOTFILE/,"root/ana_'${analysis_type}'_'${jobNo}'.root")}
-		{sub(/OUTPUT_PATH/,"'${outputFolder}'")}
-		{sub(/NEVENTS/,'${nEventsPerJob}')}
-		{if(flag == 1) {print $0} else {next} }' \
-	${templateName} > ana/ana_${analysis_type}_${jobNo}.txt
+	# * Create and EMPTY scripts directory
+	CreateOrEmptyOutputSubDir "${scriptFolder}" "sim"
+	CreateOrEmptyOutputSubDir "${scriptFolder}" "rec"
+	CreateOrEmptyOutputSubDir "${scriptFolder}" "ana"
+	CreateOrEmptyOutputSubDir "${scriptFolder}" "submit"
+	# * Create and EMPTY sutput directory
+	CreateOrEmptyOutputSubDir "${outputFolder}" "rtraw"
+	CreateOrEmptyOutputSubDir "${outputFolder}" "dst"
+	CreateOrEmptyOutputSubDir "${outputFolder}" "dst"
+	CreateOrEmptyOutputSubDir "${outputFolder}" "log"
 
-	# * Generate the submit files *
-	CheckTemplateFile "${scriptFolder}/templates/submit.sh"
-	awk '{flag = 1}
-		{sub(/SCRIPT_PATH/,"'${scriptFolder}'")}
-		{sub(/OUTPUT_PATH/,"'${outputFolder}'")}
-		{sub(/SIM_BOS/,"sim/sim_'${analysis_type}'_'${jobNo}'.txt")}
-		{sub(/SIM_LOG/,"log/sim_'${analysis_type}'_'${jobNo}'.log")}
-		{sub(/REC_BOS/,"rec/rec_'${analysis_type}'_'${jobNo}'.txt")}
-		{sub(/REC_LOG/,"log/rec_'${analysis_type}'_'${jobNo}'.log")}
-		{sub(/ANA_BOS/,"ana/ana_'${analysis_type}'_'${jobNo}'.txt")}
-		{sub(/ANA_LOG/,"log/ana_'${analysis_type}'_'${jobNo}'.log")}
-		{if(flag == 1) {print $0} else {next} }' \
-	${templateName} > submit/submit_${analysis_type}_${jobNo}.sh
+	# * Loop over jobs
+	for jobNo in $(seq 0 $((${nJobs} - 1))); do
 
-done
+		# * Generate the simulation files (sim) *
+		CheckTemplateFile "${scriptFolder}/templates/jobOptions_sim_${analysis_type}.txt"
+		awk '{flag = 1}
+			{sub(/RDF/,"rtraw/sim_'${analysis_type}'_'${jobNo}'.rtraw")}
+			{sub(/SCRIPT_PATH/,"'${scriptFolder}'")}
+			{sub(/OUTPUT_PATH/,"'${outputFolder}'")}
+			{sub(/RAND/,'$(($(date +%s%N) % 1000000000))')}
+			{sub(/DEC/,"dec/'${analysis_type}'.dec")}
+			{sub(/NEVENTS/,'${nEventsPerJob}')}
+			{if(flag == 1) {print $0} else {next} }' \
+		${templateName} > "${scriptFolder}/sim/sim_${analysis_type}_${jobNo}.txt"
+
+		# * Generate the reconstruction files (rec) *
+		CheckTemplateFile "${scriptFolder}/templates/jobOptions_rec_${analysis_type}.txt"
+		awk '{flag = 1}
+			{sub(/RDF/,"rtraw/sim_'${analysis_type}'_'${jobNo}'.rtraw")}
+			{sub(/DSTFILE/,"dst/rec_'${analysis_type}'_'${jobNo}'.dst")}
+			{sub(/OUTPUT_PATH/,"'${outputFolder}'")}
+			{sub(/RAND/,'$(($(date +%s%N) % 1000000000))')}
+			{sub(/NEVENTS/,'${nEventsPerJob}')}
+			{if(flag == 1) {print $0} else {next} }' \
+		${templateName} > "${scriptFolder}/rec/rec_${analysis_type}_${jobNo}.txt"
+
+		# * Generate the analyse files (ana) *
+		CheckTemplateFile "${scriptFolder}/templates/jobOptions_ana_${analysis_type}.txt"
+		awk '{flag = 1}
+			{sub(/DSTFILE/,"dst/rec_'${analysis_type}'_'${jobNo}'.dst")}
+			{sub(/ROOTFILE/,"root/ana_'${analysis_type}'_'${jobNo}'.root")}
+			{sub(/OUTPUT_PATH/,"'${outputFolder}'")}
+			{sub(/NEVENTS/,'${nEventsPerJob}')}
+			{if(flag == 1) {print $0} else {next} }' \
+		${templateName} > "${scriptFolder}/ana/ana_${analysis_type}_${jobNo}.txt"
+
+		# * Generate the submit files *
+		CheckTemplateFile "${scriptFolder}/templates/submit.sh"
+		awk '{flag = 1}
+			{sub(/SCRIPT_PATH/,"'${scriptFolder}'")}
+			{sub(/OUTPUT_PATH/,"'${outputFolder}'")}
+			{sub(/SIM_BOS/,"sim/sim_'${analysis_type}'_'${jobNo}'.txt")}
+			{sub(/SIM_LOG/,"log/sim_'${analysis_type}'_'${jobNo}'.log")}
+			{sub(/REC_BOS/,"rec/rec_'${analysis_type}'_'${jobNo}'.txt")}
+			{sub(/REC_LOG/,"log/rec_'${analysis_type}'_'${jobNo}'.log")}
+			{sub(/ANA_BOS/,"ana/ana_'${analysis_type}'_'${jobNo}'.txt")}
+			{sub(/ANA_LOG/,"log/ana_'${analysis_type}'_'${jobNo}'.log")}
+			{if(flag == 1) {print $0} else {next} }' \
+		${templateName} > "${scriptFolder}/submit/submit_${analysis_type}_${jobNo}.sh"
+
+	done
+
+# * ------- Final terminal output ------- * #
+	echo -e "\e[92mSuccesfully created ${nJobs} \"${analysis_type}\" job files with ${nEventsPerJob} events each\e[0m"
--- jobs/SubmitAll.sh
+++ jobs/SubmitAll.sh
@@ -1,22 +1,32 @@
 #!/bin/bash -
 # * ===============================================================================
-# *         AUTHOR: Remco de Boer (@IHEP), EMAIL: remco.de.boer@ihep.ac.cn
-# *   ORGANIZATION: IHEP, CAS (Beijing, CHINA)
-# *        CREATED: 22 October 2018
-# *          USAGE: bash SubmitAll.sh <analysis name>
-# *      ARGUMENTS: $1 analysis name (e.g. "rhopi")
+# *   DESCRIPTION: Batch submit jobOption files created with CreateJobFiles.sh
+# *        AUTHOR: Remco de Boer (@IHEP), EMAIL: remco.de.boer@ihep.ac.cn
+# *  ORGANIZATION: IHEP, CAS (Beijing, CHINA)
+# *       CREATED: 22 October 2018
+# *         USAGE: bash SubmitAll.sh <analysis name>
+# *     ARGUMENTS: $1 analysis name (e.g. "rhopi")
 # * ===============================================================================
 
 set -o nounset # Treat unset variables as an error
 
 # * ------- Script parameters ------- *
-analysis_type="${1}" # will be used in file naming
-scriptFolder="/ihepbatch/bes/$USER/workarea-7.0.3/TestRelease/TestRelease-00-00-86/run"
+	analysis_type="${1}" # will be used in file naming
+	scriptFolder="/ihepbatch/bes/$USER/workarea-7.0.3/TestRelease/TestRelease-00-00-86/run"
 
 # * ------- Run over all job files ------- *
-read -p "Submitting $(ls ${scriptFolder}/submit/submit_${analysis_type}_*.sh | wc -l) \"${analysis_type}\" jobs. Continue...?"
-for job in $(ls ${scriptFolder}/submit/submit_${analysis_type}_*.sh)
-do
-	chmod +x ${job}
-	hep_sub -g physics ${job}
-done
+	nJobs=$(ls ${scriptFolder}/submit/submit_${analysis_type}_*.sh | wc -l)
+	read -p "Submitting ${nJobs} \"${analysis_type}\" jobs. Continue...?"
+	for job in $(ls ${scriptFolder}/submit/submit_${analysis_type}_*.sh); do
+		chmod +x ${job} # not sure if necessary
+		hep_sub -g physics ${job}
+		if [ $? != 0 ]; then
+			echo -e "\e[91mAborted submitting jobs\e[0m"
+			exit
+		fi
+	done
+
+# * ------- Final terminal output ------- * #
+	echo -e "\e[92mSuccesfully submitted ${nJobs} \"${analysis_type}\" jobs\e[0m\n"
+	echo "These are your jobs:"
+	hep_q -u $USER
