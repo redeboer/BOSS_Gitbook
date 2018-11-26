06a58b19ebcb28207353f5eafde7d53f032046f0
R.E. de Boer <remco.de.boer@ihep.ac.cn>
2018-11-26 09:06:58 +0800

    Attempt to fix SubmitAll.sh

--- jobs/SubmitAll.sh
+++ jobs/SubmitAll.sh
@@ -64,10 +64,9 @@
 # * ====================================== * #
 
 	AskForInput "Press ENTER to submit ${nJobs} \"${analysisType}\" jobs..."
-	# for job in $(ls ${scriptFolder}/sub/sub_${analysisType}_*.sh); do #! absolute path
-	for job in $(ls sub/sub_${analysisType}_*.sh); do
+	for job in $(ls ${scriptFolder}/sub/sub_${analysisType}_*.sh); do #! absolute path
+	# for job in $(ls sub/sub_${analysisType}_*.sh); do #! relative path
 		chmod +x "${job}" # not sure if necessary
-		echo "${job}"
 		hep_sub -g physics "${job}"
 		if [ $? != 0 ]; then
 			PrintErrorMessage "Aborted submitting jobs"
