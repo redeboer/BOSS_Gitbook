2a79742bf81a037606c021ea648a261684c350b5
R.E. de Boer <remco.de.boer@ihep.ac.cn>
2018-11-29 16:56:20 +0800

    Added functionality for unique_ptr and added fit pars to Settings

--- inc/FrameworkSettings.h
+++ inc/FrameworkSettings.h
@@ -1,6 +1,18 @@
 #ifndef Physics_Analysis_Settings_H
 #define Physics_Analysis_Settings_H
 
+// * For backward compatibility with ROOT5 * //
+#ifdef __CINT__
+	#include <TLatex.h>
+	#define UNIQUE_PTR(TYPE) TYPE*
+	#define SHARED_PTR(TYPE) TYPE*
+	#define GET_PTR(VARIABLE) VARIABLE
+#else
+	#define SHARED_PTR(TYPE) std::shared_ptr<TYPE>
+	#define UNIQUE_PTR(TYPE) std::unique_ptr<TYPE>
+	#define GET_PTR(VARIABLE) (VARIABLE).get()
+#endif
+
 /**
  * @brief    Header with a namespace `Settings` that contains <i>all</i> settings for the analysis framework.
  * @author   Remco de Boer 雷穆克 (r.e.deboer@students.uu.nl or remco.de.boer@ihep.ac.cn)
@@ -13,22 +25,31 @@
 #include "Particle.h"
 #include "TString.h"
 
+// * In case of C++ 2011 compilation *
+namespace std_fix {
+	template <typename T, typename... Args>
+	std::unique_ptr<T> make_unique(Args&&... args)
+	{
+		return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+	}
+} // namespace std_fix
+
 namespace Settings
 {
-	namespace Output
-	{
-		const char* Extension = "pdf"; //!< Default extension of output plots (e.g. `png`, `eps`, `jpg`, etc.).
-		const TString PlotOutputDir = Form("../plots/%s", Extension); //!< Default output directory for plots.
-	}
-	namespace Math
-	{
-		const double TwoPi = 6.2831853; //!< Mathematical constant \f$ 2 pi\f$.
-		const double Pi    = 3.1415927; //!< Mathematical constant \f$ pi\f$.
-	}
 	namespace Detector
 	{
 		const double TofUnitCm = 29.9792458; //!< Time of Flight path unit in cm.
 		const double TofUnitMm = 299.792458; //!< Time of Flight path unit in mm.
+	}
+	namespace Fit
+	{
+		const double fSigmaScaleFactorLow = 0.; //!< This `double` parametr will be used when constructing a `RooRealVar` object for the sigma's of the double Gaussian. The lower boundary of this `RooRealVar` sigma object will be set to this factor times the estimated sigma).
+		const double fSigmaScaleFactorUp = 10.; //!< This `double` parameter will be used when constructing a `RooRealVar` object for the sigma's of the double Gaussian. The upper boundary of this `RooRealVar` sigma object will be set to this factor times the estimated sigma).
+	}
+	namespace Output
+	{
+		const char* Extension = "pdf"; //!< Default extension of output plots (e.g. `png`, `eps`, `jpg`, etc.).
+		const TString PlotOutputDir = Form("../plots/%s", Extension); //!< Default output directory for plots.
 	}
 	/**
 	 * @brief List of relevant particles used in this analysis.
