a2d420bcedf822708b7f214090948ff27b7b3912
R.E. de Boer <remco.de.boer@ihep.ac.cn>
2018-12-03 16:29:35 +0800

    Added a BOSS package (not yet tested)

---
+++ boss/Physics/RhopiAlg/src/components/Rhopi_entries.cxx
@@ -0,0 +1,9 @@
+#include "GaudiKernel/DeclareFactoryEntries.h"
+#include "RhopiAlg/Rhopi.h"
+
+DECLARE_ALGORITHM_FACTORY( Rhopi )
+
+DECLARE_FACTORY_ENTRIES( RhopiAlg ) {
+  DECLARE_ALGORITHM(Rhopi);
+}
+
