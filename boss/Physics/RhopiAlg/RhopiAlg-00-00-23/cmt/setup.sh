724dc20054661c628fdcd68b99c540ce2995b5ac
R.E. de Boer <remco.de.boer@ihep.ac.cn>
2018-12-09 10:56:00 +0800

    Added RhopiAlg back (was removed for some reason)

---
+++ boss/Physics/RhopiAlg/RhopiAlg-00-00-23/cmt/setup.sh
@@ -0,0 +1,25 @@
+# echo "setup RhopiAlg RhopiAlg-00-00-23 in /besfs/users/$USER/BOSS_Afterburner/boss/Physics"
+
+if test "${CMTROOT}" = ""; then
+  CMTROOT=/afs/ihep.ac.cn/bes3/offline/ExternalLib/SLC6/contrib/CMT/v1r25; export CMTROOT
+fi
+. ${CMTROOT}/mgr/setup.sh
+cmtRhopiAlgtempfile=`${CMTROOT}/mgr/cmt -quiet build temporary_name`
+if test ! $? = 0 ; then cmtRhopiAlgtempfile=/tmp/cmt.$$; fi
+${CMTROOT}/mgr/cmt setup -sh -pack=RhopiAlg -version=RhopiAlg-00-00-23 -path=/besfs/users/$USER/BOSS_Afterburner/boss/Physics  -no_cleanup $* >${cmtRhopiAlgtempfile}
+if test $? != 0 ; then
+  echo >&2 "${CMTROOT}/mgr/cmt setup -sh -pack=RhopiAlg -version=RhopiAlg-00-00-23 -path=/besfs/users/$USER/BOSS_Afterburner/boss/Physics  -no_cleanup $* >${cmtRhopiAlgtempfile}"
+  cmtsetupstatus=2
+  /bin/rm -f ${cmtRhopiAlgtempfile}
+  unset cmtRhopiAlgtempfile
+  return $cmtsetupstatus
+fi
+cmtsetupstatus=0
+. ${cmtRhopiAlgtempfile}
+if test $? != 0 ; then
+  cmtsetupstatus=2
+fi
+/bin/rm -f ${cmtRhopiAlgtempfile}
+unset cmtRhopiAlgtempfile
+return $cmtsetupstatus
+
