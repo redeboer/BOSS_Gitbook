770964cefa45769bf49744ad0c36c817e13c9a28
redeboer <r.e.deboer@students.uu.nl>
2018-12-11 09:39:10 +0800

    Modified .load_boss.sh

--- boss/.load_boss.sh
+++ boss/.load_boss.sh
@@ -1,15 +1,16 @@
-# * Some folder paths * #
+# * Some BOSS variables * #
 BESFS="/besfs/users/${USER}"
-IHEPBATCH="/ihepbatch/bes/${USER}"
-BOSS_Afterburner="${BESFS}/BOSS_Afterburner"
-
-# * Setup BOSS area * #
 BOSSVERSION="7.0.4"
+BOSS_Afterburner="${BESFS}/BOSS_Afterburner"
 BOSSWORKAREA="${BOSS_Afterburner}/boss"
 BOSS_SOURCE="/afs/ihep.ac.cn/bes3/offline/Boss/cmthome/cmthome-${BOSSVERSION}"
 CMTHOME="/afs/ihep.ac.cn/bes3/offline/Boss/cmthome/cmthome-${BOSSVERSION}"
 CMTHOMENAME="cmthome"
+IHEPBATCH="/ihepbatch/bes/${USER}"
+SCRATCHFS="/scratchfs/bes/${USER}"
 WORKAREANAME="workarea"
+
+# * Setup BOSS area * #
 source "${BOSSWORKAREA}/${CMTHOMENAME}/setupCMT.sh"
 source "${BOSSWORKAREA}/${CMTHOMENAME}/setup.sh"
 source "${BOSSWORKAREA}/${WORKAREANAME}/TestRelease/TestRelease-"*"/cmt/setup.sh"
