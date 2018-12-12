0116581fab0c0ff1f884f406bd58af5ac9c7f7b5
R.E. de Boer <remco.de.boer@ihep.ac.cn>
2018-12-12 16:39:48 +0800

    Duplicated versions for TestRelease and RhopiAlg

---
+++ boss/workarea/TestRelease/TestRelease-00-01-00/cmt/cleanup.sh
@@ -0,0 +1,25 @@
+# echo "cleanup TestRelease TestRelease-00-00-86 in /afs/.ihep.ac.cn/bes3/offline/Boss/7.0.4"
+
+if test "${CMTROOT}" = ""; then
+  CMTROOT=/afs/ihep.ac.cn/bes3/offline/ExternalLib/SLC6/contrib/CMT/v1r25; export CMTROOT
+fi
+. ${CMTROOT}/mgr/setup.sh
+cmtTestReleasetempfile=`${CMTROOT}/mgr/cmt -quiet build temporary_name`
+if test ! $? = 0 ; then cmtTestReleasetempfile=/tmp/cmt.$$; fi
+${CMTROOT}/mgr/cmt cleanup -sh -pack=TestRelease -version=TestRelease-00-00-86 -path=/afs/.ihep.ac.cn/bes3/offline/Boss/7.0.4  $* >${cmtTestReleasetempfile}
+if test $? != 0 ; then
+  echo >&2 "${CMTROOT}/mgr/cmt cleanup -sh -pack=TestRelease -version=TestRelease-00-00-86 -path=/afs/.ihep.ac.cn/bes3/offline/Boss/7.0.4  $* >${cmtTestReleasetempfile}"
+  cmtcleanupstatus=2
+  /bin/rm -f ${cmtTestReleasetempfile}
+  unset cmtTestReleasetempfile
+  return $cmtcleanupstatus
+fi
+cmtcleanupstatus=0
+. ${cmtTestReleasetempfile}
+if test $? != 0 ; then
+  cmtcleanupstatus=2
+fi
+/bin/rm -f ${cmtTestReleasetempfile}
+unset cmtTestReleasetempfile
+return $cmtcleanupstatus
+
