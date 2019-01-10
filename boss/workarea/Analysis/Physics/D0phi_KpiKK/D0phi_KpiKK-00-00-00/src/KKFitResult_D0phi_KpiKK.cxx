a4da334b1c7b8c34d8205bd7ad462771e994187b
redeboer <r.e.deboer@students.uu.nl>
2019-01-10 13:11:15 +0800

    Updated Doxygen

--- boss/workarea/Analysis/Physics/D0phi_KpiKK/D0phi_KpiKK-00-00-00/src/KKFitResult_D0phi_KpiKK.cxx
+++ boss/workarea/Analysis/Physics/D0phi_KpiKK/D0phi_KpiKK-00-00-00/src/KKFitResult_D0phi_KpiKK.cxx
@@ -33,32 +33,34 @@
 // * ============================ * //
 
 	/**
-	 * @brief Constructof for the `KKFitResult` base class.
+	 * @brief Helper function for the constructor (hence `private` method).
 	 */
 	void KKFitResult_D0phi_KpiKK::SetValues(KalmanKinematicFit* kkmfit)
 	{
 		/// <ol>
 		/// <li> Test whether `KalmanKinematicFit` pointer exists.
 		if(!fFit) return;
-		/// <li> Get Lorentz vectors of the decay products:
-			/// <ol>
+		/// <li> Get Lorentz vectors of the decay products using `KalmanKinematicFit::pfit`:
+			/// <ul>
 			HepLorentzVector pKaonNeg1 =  fFit->pfit(0); /// <li> \f$K^-\f$ (first occurrence)
 			HepLorentzVector pKaonNeg2 =  fFit->pfit(1); /// <li> \f$K^-\f$ (second occurrence)
 			HepLorentzVector pKaonPos  =  fFit->pfit(2); /// <li> \f$K^+\f$
 			HepLorentzVector pPionPos  =  fFit->pfit(3); /// <li> \f$\pi^+\f$
-			/// </ol>
+			/// </ul>
 		/// <li> Compute Lorentz vectors of the particles to be reconstructed:
-			/// <ol>
+			/// <ul>
 			HepLorentzVector pD0   = pKaonNeg1 + pPionPos; /// <li> \f$D^0 \rightarrow K^-\pi^+\f$
 			HepLorentzVector pphi  = pKaonNeg2 + pKaonPos; /// <li> \f$\phi \rightarrow K^-K^+\f$
 			HepLorentzVector pJpsi = pD0 + pphi;           /// <li> \f$J/\psi \rightarrow D^0\phi\f$
-			/// </ol>
+			/// </ul>
 		/// <li> Compute invariant masses and momentum:
+			/// <ul>
 			fM_D0   = pD0.m();    /// <li> `fM_D0`   = \f$M_{K^-\pi^+}\f$
 			fM_phi  = pphi.m();   /// <li> `fM_phi`  = \f$M_{K^-K^+}\f$
 			fM_Jpsi = pJpsi.m();  /// <li> `fM_Jpsi` = \f$M_{D^0\phi}\f$
 			fP_D0   = std::sqrt(pD0 .px()*pD0 .px() + pD0 .py()*pD0 .py() + pD0 .pz()*pD0 .pz());  /// <li> `fP_D0`  = \f$|\vec{p}_{K^-\pi^+}|\f$
 			fP_phi  = std::sqrt(pphi.px()*pphi.px() + pphi.py()*pphi.py() + pphi.pz()*pphi.pz());  /// <li> `fP_phi` = \f$|\vec{p}_{K^-K^+}|\f$
+			/// </ul>
 		/// <li> Compute measure for best fit: `fFitMeasure` := \f$M_{K^-K^+} - m_{\phi}\f$
 		fFitMeasure = std::abs(fM_phi - gM_phi);
 		/// </ol>
