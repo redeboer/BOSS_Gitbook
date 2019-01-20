63772ed244d8d3f4f27b088ffc99bd01875401dc
redeboer <r.e.deboer@students.uu.nl>
2019-01-20 14:22:30 +0800

    Completed documentation for MctrutForTopoAna

--- boss/workarea/Analysis/TopoAna/MctruthForTopoAnaAlg/MctruthForTopoAnaAlg-00-00-00/MctruthForTopoAnaAlg/MctruthForTopoAna.h
+++ boss/workarea/Analysis/TopoAna/MctruthForTopoAnaAlg/MctruthForTopoAnaAlg-00-00-00/MctruthForTopoAnaAlg/MctruthForTopoAna.h
@@ -6,7 +6,7 @@
 
 /**
  * @brief Helper class for the `topoana` package.
- * @details It is essential to run load this algorithm along with your own algorithm if you want to run `topoana.exe` over the output of your initial event selection. This algorithm (`MctruthForTopoAna`) will store the relevant 'MC truth' information.
+ * @details It is essential to run load this algorithm along with your own algorithm if you want to run `topoana.exe` over the output of your initial event selection. This algorithm (`MctruthForTopoAna`) will store the relevant 'Monte Carlo truth' information.
  * @remark January 21st, 2019: *This procedure has been implemented in the `TrackSelector` and does not have to be loaded anymore.
  * @author Zhou Xingyu 周兴玉
  */
@@ -18,8 +18,8 @@ public:
 	StatusCode execute();
 	StatusCode finalize();
 private:
-	NTuple::Tuple * m_tuple;
-	NTuple::Item<int>  m_ievt;
+	NTuple::Tuple * m_tuple; //!< `NTuple` that will be the eventual `"MctruthForTopoAna"` tree.
+	NTuple::Item<int>  m_ievt; //!< 
 	NTuple::Item<int>  m_runid;
 	NTuple::Item<int>  m_evtid;
 	NTuple::Item<int>  m_nmcps;
--- boss/workarea/Analysis/TopoAna/MctruthForTopoAnaAlg/MctruthForTopoAnaAlg-00-00-00/src/MctruthForTopoAna.cxx
+++ boss/workarea/Analysis/TopoAna/MctruthForTopoAnaAlg/MctruthForTopoAnaAlg-00-00-00/src/MctruthForTopoAna.cxx
@@ -24,32 +24,40 @@
 // * ------- ALGORITHM STEPS ------- * //
 // * =============================== * //
 
+
 	/**
 	 * @brief Book `NTuple` for MC truth.
 	 * @details The contents of this `NTuple` are required for the `topoana` package to function properly.
 	 */
 	StatusCode MctruthForTopoAna::initialize()
 	{
-		MsgStream log(msgSvc(),name());
-		log << MSG::INFO << "in initialize()" << endmsg;
-		StatusCode status;
-
-		NTuplePtr nt(ntupleSvc(),"FILE1/MctruthForTopoAna");
-		if(nt) m_tuple=nt;
-		else {
-			m_tuple=ntupleSvc()->book("FILE1/MctruthForTopoAna",CLID_ColumnWiseTuple,"Mctruth For Topology Analysis");
-			if(m_tuple) {
-				status=m_tuple->addItem("Ievt",m_ievt);
-				status=m_tuple->addItem("Runid",m_runid);
-				status=m_tuple->addItem("Evtid",m_evtid);
-				status=m_tuple->addItem("Nmcps",m_nmcps,0,100);
-				status=m_tuple->addIndexedItem("Pid",m_nmcps,m_pid);
-				status=m_tuple->addIndexedItem("Midx",m_nmcps,m_midx);
-			} else {
-				log << MSG::ERROR << "Cannot book N-tuple:" << long(m_tuple) << endmsg;
-				return StatusCode::FAILURE;
+		/// <ol>
+		/// <li> Create log stream (`MsgStream` object).
+			MsgStream log(msgSvc(),name());
+			log << MSG::INFO << "in initialize()" << endmsg;
+		/// <li> Book `NTuple` called `"MctruthForTopoAna"`. A selection of MC truth data will be written to this `TTree`. This tree contains:
+			NTuplePtr nt(ntupleSvc(),"FILE1/MctruthForTopoAna");
+			if(nt) m_tuple=nt;
+			else {
+				m_tuple=ntupleSvc()->book("FILE1/MctruthForTopoAna",CLID_ColumnWiseTuple,"Mctruth For Topology Analysis");
+				if(m_tuple) {
+				/// <ul>
+					m_tuple->addItem("Ievt",  m_ievt);  /// <li> `"Ievt"`: <b>counter</b> for number of events (not the ID!).
+					m_tuple->addItem("Runid", m_runid); /// <li> `"Runid"`: run number ID.
+					m_tuple->addItem("Evtid", m_evtid); /// <li> `"Evtid"`: event number ID.
+					m_tuple->addItem("Nmcps", m_nmcps, 0, 100); /// <li> `"Nmcps"`: number of MC particles stored for this event. This one is necessary for loading following two items, because they are arrays.
+					m_tuple->addIndexedItem("Pid",  m_nmcps, m_pid); /// <li> `"Pid"` (array): PDG code for the particle in this array.
+					m_tuple->addIndexedItem("Midx", m_nmcps, m_midx); /// <li> `"Midx"` (array): track index of the mother particle (corrected with `rootIndex`).
+				/// </ul>
+				} else {
+					log << MSG::ERROR << "Cannot book N-tuple:" << long(m_tuple) << endmsg;
+					/// <li> Abort if booking the `NTuple` has failed.
+					return StatusCode::FAILURE;
+				}
 			}
-		}
+		/// <li> Reset counter for number of events.
+			m_ievt = 0;
+		/// </ol>
 		return StatusCode::SUCCESS;
 	}
 
@@ -60,48 +68,70 @@
 	int ievt=0;
 	StatusCode MctruthForTopoAna::execute() 
 	{
-		ievt++;
-		m_ievt=ievt;
-		// std::cout << std::endl << std::endl;
-		// std::cout << "ievt=" << ievt << std::endl;
-		SmartDataPtr<Event::EventHeader> eventHeader(eventSvc(),"/Event/EventHeader");
-		// std::cout << "RunNo=" << eventHeader->runNumber() << std::endl;
-		// std::cout << "EvtNo=" << eventHeader->eventNumber() << std::endl;
-		m_runid=eventHeader->runNumber();
-		m_evtid=eventHeader->eventNumber();
+		/// <ol>
+		/// <li> Create log stream object and increase counter for number of events.
+			MsgStream log(msgSvc(), name());
+			++m_ievt;
+			// std::cout << std::endl << std::endl;
+			// std::cout << "ievt = " << ievt << std::endl;
 
-		SmartDataPtr<Event::McParticleCol> mcParticleCol(eventSvc(),"/Event/MC/McParticleCol");
-		if(!mcParticleCol) {
-			std::cout << "Could not retrieve McParticelCol" << std::endl;
-			return(StatusCode::FAILURE);  
-		}
-		Event::McParticleCol::iterator iter_mc=mcParticleCol->begin();
-		m_nmcps = 0;
-		const int incPid1 = 91; // 91 is the PDG code of cluster
-		const int incPid2 = 92; // 92 is the PDG code of string
-		bool incPdcy(false);
-		int rootIndex(-1);
-		for(; iter_mc!=mcParticleCol->end(); iter_mc++) {
-			// std::cout << "idx=" << m_nmcps << "\t" << "pid=" << (*iter_mc)->particleProperty() << "\t" << "midx=" << ((*iter_mc)->mother()).trackIndex() << std::endl;
-			if((*iter_mc)->primaryParticle()) continue;
-			if(!(*iter_mc)->decayFromGenerator()) continue;
-			if(
-				(*iter_mc)->particleProperty()==incPid1 ||
-				(*iter_mc)->particleProperty()==incPid2 ) {
-				incPdcy=true;
-				rootIndex=(*iter_mc)->trackIndex();
+		/// <li> Load event from DST file and get run and event number.
+			SmartDataPtr<Event::EventHeader> eventHeader(eventSvc(),"/Event/EventHeader");
+			m_runid=eventHeader->runNumber();
+			m_evtid=eventHeader->eventNumber();
+			// std::cout << "RunNo=" << m_runid << std::endl;
+			// std::cout << "EvtNo=" << m_evtid << std::endl;
+
+		/// <li> Attempt to get 'Monte Carlo truth'.
+			SmartDataPtr<Event::McParticleCol> mcParticleCol(eventSvc(),"/Event/MC/McParticleCol");
+			if(!mcParticleCol) {
+				std::cout << "Could not retrieve McParticelCol" << std::endl;
+				return(StatusCode::FAILURE);  
 			}
-			if(!incPdcy ||
-				(*iter_mc)->particleProperty()==incPid1 ||
-				(*iter_mc)->particleProperty()==incPid2 ) continue;
-			m_pid[m_nmcps]=(*iter_mc)->particleProperty();
-			if(
-				((*iter_mc)->mother()).particleProperty()==incPid1 ||
-				((*iter_mc)->mother()).particleProperty()==incPid2 ) m_midx[m_nmcps]=-1;
-			else m_midx[m_nmcps]=((*iter_mc)->mother()).trackIndex()-rootIndex-1;
-			m_nmcps++;
-		}
-		m_tuple->write();
+
+		/// <li> Attempt to get 'Monte Carlo truth'.
+			/// <ul>
+			m_nmcps = 0; /// <li> Reset the `m_nmcps` counter for the number of MC particles.
+			const int incPid1 = 91; /// <li> `91` is the PDG code of cluster.
+			const int incPid2 = 92; /// <li> `92` is the PDG code of string.
+			bool incPdcy(false);
+			int rootIndex(-1);
+			/// </ul>
+
+		/// <li> Loop over collection of MC particle objects. An array containing a selection of MC truth particles is created here.
+			/// <ul>
+			Event::McParticleCol::iterator it = mcParticleCol->begin();
+			for(; it!=mcParticleCol->end(); it++) {
+				// std::cout << "idx=" << m_nmcps << "\t" << "pid=" << (*it)->particleProperty() << "\t" << "midx=" << ((*it)->mother()).trackIndex() << std::endl;
+				/// <li> Only decayed particles are of interest to the `topoana` package.
+				if((*it)->primaryParticle()) continue;
+				if(!(*it)->decayFromGenerator()) continue;
+				/// <li> The `rootIndex` variable is used to skip particles like the \f$Z\f$ boson in the decay chain after the \f$e^+e^-\f$ collision. It has to be stored once and untill it is stored, all MC truth particles are rejected (which is what the `incPdcy` boolean is for).
+				if(
+					(*it)->particleProperty() == incPid1 ||
+					(*it)->particleProperty() == incPid2 ) {
+					incPdcy = true;
+					rootIndex = (*it)->trackIndex();
+				}
+				/// <li> Skip if this particle is the initial cluster.
+				if(!incPdcy ||
+					(*it)->particleProperty() == incPid1 ||
+					(*it)->particleProperty() == incPid2 ) continue;
+				/// <li> Store PDG code of this MC particle.
+				m_pid[m_nmcps] = (*it)->particleProperty();
+				/// <li> Store track ID of the mother. This ID is `-1` if it is the initial cluster (e.g. \f$J/\psi\f$).
+				if(
+					((*it)->mother()).particleProperty() == incPid1 ||
+					((*it)->mother()).particleProperty() == incPid2 ) m_midx[m_nmcps] = -1;
+				else m_midx[m_nmcps] = ((*it)->mother()).trackIndex()-rootIndex-1;
+				m_nmcps++;
+			}
+			/// </ul>
+
+		/// <li> <b>Write</b> info stored in the loop to the `"MctruthForTopoAna"` `TTree`.
+			m_tuple->write();
+
+		/// </ol>
 		return StatusCode::SUCCESS;
 	}
 
