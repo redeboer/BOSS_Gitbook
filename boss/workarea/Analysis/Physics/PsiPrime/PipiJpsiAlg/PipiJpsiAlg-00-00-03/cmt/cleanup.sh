55bfc13417655b045b49817d4b780b91de4b7a43
redeboer <r.e.deboer@students.uu.nl>
2019-02-12 17:29:36 +0800

    Test compile PipiJpsiAlg

--- boss/workarea/Analysis/Physics/PsiPrime/PipiJpsiAlg/PipiJpsiAlg-00-00-03/cmt/cleanup.sh
+++ boss/workarea/Analysis/Physics/PsiPrime/PipiJpsiAlg/PipiJpsiAlg-00-00-03/cmt/cleanup.sh
@@ -1,4 +1,4 @@
-# echo "cleanup PipiJpsiAlg PipiJpsiAlg-00-00-03 in /afs/.ihep.ac.cn/bes3/offline/Boss/7.0.4/Analysis/Physics/PsiPrime"
+# echo "cleanup PipiJpsiAlg PipiJpsiAlg-00-00-03 in /besfs/users/deboer/BOSS_Afterburner/boss/workarea/Analysis/Physics/PsiPrime"
 
 if test "${CMTROOT}" = ""; then
   CMTROOT=/afs/ihep.ac.cn/bes3/offline/ExternalLib/SLC6/contrib/CMT/v1r25; export CMTROOT
@@ -6,9 +6,9 @@ fi
 . ${CMTROOT}/mgr/setup.sh
 cmtPipiJpsiAlgtempfile=`${CMTROOT}/mgr/cmt -quiet build temporary_name`
 if test ! $? = 0 ; then cmtPipiJpsiAlgtempfile=/tmp/cmt.$$; fi
-${CMTROOT}/mgr/cmt cleanup -sh -pack=PipiJpsiAlg -version=PipiJpsiAlg-00-00-03 -path=/afs/.ihep.ac.cn/bes3/offline/Boss/7.0.4/Analysis/Physics/PsiPrime  $* >${cmtPipiJpsiAlgtempfile}
+${CMTROOT}/mgr/cmt cleanup -sh -pack=PipiJpsiAlg -version=PipiJpsiAlg-00-00-03 -path=/besfs/users/deboer/BOSS_Afterburner/boss/workarea/Analysis/Physics/PsiPrime  $* >${cmtPipiJpsiAlgtempfile}
 if test $? != 0 ; then
-  echo >&2 "${CMTROOT}/mgr/cmt cleanup -sh -pack=PipiJpsiAlg -version=PipiJpsiAlg-00-00-03 -path=/afs/.ihep.ac.cn/bes3/offline/Boss/7.0.4/Analysis/Physics/PsiPrime  $* >${cmtPipiJpsiAlgtempfile}"
+  echo >&2 "${CMTROOT}/mgr/cmt cleanup -sh -pack=PipiJpsiAlg -version=PipiJpsiAlg-00-00-03 -path=/besfs/users/deboer/BOSS_Afterburner/boss/workarea/Analysis/Physics/PsiPrime  $* >${cmtPipiJpsiAlgtempfile}"
   cmtcleanupstatus=2
   /bin/rm -f ${cmtPipiJpsiAlgtempfile}
   unset cmtPipiJpsiAlgtempfile
