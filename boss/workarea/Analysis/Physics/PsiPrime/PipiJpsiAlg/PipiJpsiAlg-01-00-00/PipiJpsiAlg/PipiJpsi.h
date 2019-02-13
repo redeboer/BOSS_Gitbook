5792b25b8ea28c94753594bce48494aef690b122
redeboer <r.e.deboer@students.uu.nl>
2019-02-13 11:35:47 +0800

    Updated Doxygen

--- boss/workarea/Analysis/Physics/PsiPrime/PipiJpsiAlg/PipiJpsiAlg-01-00-00/PipiJpsiAlg/PipiJpsi.h
+++ boss/workarea/Analysis/Physics/PsiPrime/PipiJpsiAlg/PipiJpsiAlg-01-00-00/PipiJpsiAlg/PipiJpsi.h
@@ -1,20 +1,6 @@
 #ifndef Physics_Analysis_PipiJpsi_H
 #define Physics_Analysis_PipiJpsi_H 
 
-/// @addtogroup BOSS_packages
-/// @{
-
-/**
- * @brief    Modified header of the stock BOSS package `PipiJpsiAlg` (version `00-00-03`).
- * @details Analyse \f$ \psi' \rightarrow J/\psi \pi \pi \f$ and \f$ J/\psi \rightarrow \mathrm{di-leptons} \f$ events. This example package also teaches the use of Monte Carlo truth.
- *
- * @author   Kai Zhu (zhuk@ihep.ac.cn)
- * @author   Remco de Boer 雷穆克 (r.e.deboer@students.uu.nl or remco.de.boer@ihep.ac.cn)
- * @date     February 13th, 2019
- */
-
-
-
 // * ========================= * //
 // * ------- LIBRARIES ------- * //
 // * ========================= * //
@@ -28,167 +14,191 @@
 // * ================================ * //
 // * ------- CLASS DEFINITION ------- * //
 // * ================================ * //
-class PipiJpsi : public Algorithm {
-public:
-	// * Constructor and destructors *
-	PipiJpsi(const std::string& name, ISvcLocator* pSvcLocator);
+/// @addtogroup BOSS_packages
+/// @{
 
-	// * Algorithm steps *
-	StatusCode initialize();
-	StatusCode execute();
-	StatusCode finalize();  
 
-private:
+	/**
+	 * @brief    Modified header of the stock BOSS package `PipiJpsi` (version `00-00-03`).
+	 * @details Analyse \f$ \psi' \rightarrow J/\psi \pi \pi \f$ and \f$ J/\psi \rightarrow \mathrm{di-leptons} \f$ events. This example package also teaches the use of Monte Carlo truth.
+	 *
+	 * @author   Kai Zhu (zhuk@ihep.ac.cn)
+	 * @author   Remco de Boer 雷穆克 (r.e.deboer@students.uu.nl or remco.de.boer@ihep.ac.cn)
+	 * @date     February 13th, 2019
+	 */
+	class PipiJpsi : public Algorithm {
+	public:
+		/// @name Constructors
+			///@{
+			PipiJpsi(const std::string& name, ISvcLocator* pSvcLocator);
+			///@}
 
+		/// @name Algorithm steps
+			///@{
+			StatusCode initialize();
+			StatusCode execute();
+			StatusCode finalize();  
+			///@}
+
+	private:
+	// ! ================================= ! //
 	// ! ------- DECLARE CUTS HERE ------- ! //
-		// Here, you can define data members that you use to define cuts. The values for these cuts should be set in the `RhopiAlg::RhopiAlg` constructor (see `.cxx` file).
+	// ! ================================= ! //
+		/// @name Cut parameters
+		/// @details Here, you can define data members that you use to define cuts. The values for these cuts should be set in the `PipiJpsi::PipiJpsi` constructor (see `.cxx` file)
+			///@{
+			// * Declare r0, z0 cut for charged tracks *
+				double m_vr0cut;
+				double m_vz0cut;
 
-		// * Declare r0, z0 cut for charged tracks *
-		double m_vr0cut;
-		double m_vz0cut;
+			// * Declare whether or not to check success of Particle Identification *
+				bool m_checkDedx;
+				bool m_checkTof;
+				bool m_eventrate;
+				int  m_chan_det;
 
-		// * Declare whether or not to check success of Particle Identification *
-		bool m_checkDedx;
-		bool m_checkTof;
-		bool m_eventrate;
-		int  m_chan_det;
+			// * Declare the track angle cut and track's transverse momentum *
+				double m_cosThetaCut;  
 
-		// * Declare the track angle cut and track's transverse momentum *
-		double m_cosThetaCut;  
+			// * Declare the cut angle between two pions to drop gamma conversion *
+				double m_pipi_dang_cut;
 
-		// * Declare the cut angle between two pions to drop gamma conversion *
-		double m_pipi_dang_cut;
+			// * Declare whether pick up sub-sample *
+				bool m_subsample_flag;
+				bool m_trigger_flag;
 
-		// * Declare whether pick up sub-sample *
-		bool m_subsample_flag;
-		bool m_trigger_flag;
+			// * Declare energy/moentum to distinguish e and muon *
+				double m_distin_emuon;
 
-		// * Declare energy/moentum to distinguish e and muon *
-		double m_distin_emuon;
+			///@}
 
+
+	// ! ==================================== ! //
 	// ! ------- DECLARE NTUPLES HERE ------- ! //
-		// NTuples are like vectors, but its members do not necessarily have to be of the same type. In this package, the NTuples are used to store event-by-event information. Its values are then written to the output ROOT file, creating a ROOT TTree. In that sense, each NTuple here represents one TTree within that output ROOT file, and each NTuple::Item represents its leaves. The name of the leaves is determined when calling NTuple::addItem.
-		// Note that the NTuple::Items are added to the NTuple during the RhopiAlg::initialize() step. This is also the place where you name these variables, so make sure that the structure here is reflected there!
+	// ! ==================================== ! //
+		/// @name `NTuple`s (eventual `TTree`s and their branches)
+		/// @details `NTuple`s are like vectors, but its members do not necessarily have to be of the same type. In this package, the NTuples are used to store event-by-event information. Its values are then written to the output ROOT file, creating a ROOT TTree. In that sense, each NTuple here represents one TTree within that output ROOT file, and each NTuple::Item represents its leaves. The name of the leaves is determined when calling `NTuple::addItem`. Note that the NTuple::Items are added to the NTuple during the `PipiJpsi::initialize()` step. This is also the place where you name these variables, so make sure that the structure here is reflected there!
+			///@{
+			// * Vertex information of the charged tracks *
+				NTuple::Tuple* m_tuple1;
+				NTuple::Item<double>  m_vx0;
+				NTuple::Item<double>  m_vy0;
+				NTuple::Item<double>  m_vz0;
+				NTuple::Item<double>  m_vr0;
 
-		// * Vertex information of the charged tracks *
-		NTuple::Tuple*  m_tuple1;
-			NTuple::Item<double>  m_vx0;
-			NTuple::Item<double>  m_vy0;
-			NTuple::Item<double>  m_vz0;
-			NTuple::Item<double>  m_vr0;
+			// * 'Fake' photon (angles) *
+				NTuple::Tuple* m_tuple2;
+				NTuple::Item<double>  m_dthe;
+				NTuple::Item<double>  m_dphi;
+				NTuple::Item<double>  m_dang;
+				NTuple::Item<double>  m_eraw;
+				NTuple::Item<long>  m_nGam;
 
-		// * 'Fake' photon (angles) *
-		NTuple::Tuple*  m_tuple2;
-			NTuple::Item<double>  m_dthe;
-			NTuple::Item<double>  m_dphi;
-			NTuple::Item<double>  m_dang;
-			NTuple::Item<double>  m_eraw;
-			NTuple::Item<long>  m_nGam;
+			// * Energy loss dE/dx info *
+				NTuple::Tuple* m_tuple3;
+				NTuple::Item<double> m_ptrk;
+				NTuple::Item<double> m_chie;
+				NTuple::Item<double> m_chimu;
+				NTuple::Item<double> m_chipi;
+				NTuple::Item<double> m_chik;
+				NTuple::Item<double> m_chip;
+				NTuple::Item<double> m_probPH;
+				NTuple::Item<double> m_normPH;
+				NTuple::Item<double> m_ghit;
+				NTuple::Item<double> m_thit;
 
-		// * Energy loss dE/dx info *
-		NTuple::Tuple* m_tuple3;
-			NTuple::Item<double> m_ptrk;
-			NTuple::Item<double> m_chie;
-			NTuple::Item<double> m_chimu;
-			NTuple::Item<double> m_chipi;
-			NTuple::Item<double> m_chik;
-			NTuple::Item<double> m_chip;
-			NTuple::Item<double> m_probPH;
-			NTuple::Item<double> m_normPH;
-			NTuple::Item<double> m_ghit;
-			NTuple::Item<double> m_thit;
+			// * Endcap ToF *
+				NTuple::Tuple* m_tuple4;
+				NTuple::Item<double> m_ptot_etof;
+				NTuple::Item<double> m_cntr_etof;
+				NTuple::Item<double> m_path_etof;
+				NTuple::Item<double> m_tof_etof;
+				NTuple::Item<double> m_te_etof;
+				NTuple::Item<double> m_tmu_etof;
+				NTuple::Item<double> m_tpi_etof;
+				NTuple::Item<double> m_tk_etof;
+				NTuple::Item<double> m_tp_etof;
+				NTuple::Item<double> m_ph_etof;
+				NTuple::Item<double> m_rhit_etof;
+				NTuple::Item<double> m_qual_etof;
 
-		// * Endcap ToF *
-		NTuple::Tuple* m_tuple4;
-			NTuple::Item<double> m_ptot_etof;
-			NTuple::Item<double> m_cntr_etof;
-			NTuple::Item<double> m_path_etof;
-			NTuple::Item<double> m_tof_etof;
-			NTuple::Item<double> m_te_etof;
-			NTuple::Item<double> m_tmu_etof;
-			NTuple::Item<double> m_tpi_etof;
-			NTuple::Item<double> m_tk_etof;
-			NTuple::Item<double> m_tp_etof;
-			NTuple::Item<double> m_ph_etof;
-			NTuple::Item<double> m_rhit_etof;
-			NTuple::Item<double> m_qual_etof;
+			// * Barrel inner ToF *
+				NTuple::Tuple* m_tuple5;
+				NTuple::Item<double> m_ptot_btof1;
+				NTuple::Item<double> m_cntr_btof1;
+				NTuple::Item<double> m_path_btof1;
+				NTuple::Item<double> m_tof_btof1;
+				NTuple::Item<double> m_te_btof1;
+				NTuple::Item<double> m_tmu_btof1;
+				NTuple::Item<double> m_tpi_btof1;
+				NTuple::Item<double> m_tk_btof1;
+				NTuple::Item<double> m_tp_btof1;
+				NTuple::Item<double> m_ph_btof1;
+				NTuple::Item<double> m_zhit_btof1;
+				NTuple::Item<double> m_qual_btof1;
 
-		// * Barrel inner ToF *
-		NTuple::Tuple* m_tuple5;
-			NTuple::Item<double> m_ptot_btof1;
-			NTuple::Item<double> m_cntr_btof1;
-			NTuple::Item<double> m_path_btof1;
-			NTuple::Item<double> m_tof_btof1;
-			NTuple::Item<double> m_te_btof1;
-			NTuple::Item<double> m_tmu_btof1;
-			NTuple::Item<double> m_tpi_btof1;
-			NTuple::Item<double> m_tk_btof1;
-			NTuple::Item<double> m_tp_btof1;
-			NTuple::Item<double> m_ph_btof1;
-			NTuple::Item<double> m_zhit_btof1;
-			NTuple::Item<double> m_qual_btof1;
-
-		// * Barrel outer ToF *
-		NTuple::Tuple* m_tuple6;
-			NTuple::Item<double> m_ptot_btof2;
-			NTuple::Item<double> m_cntr_btof2;
-			NTuple::Item<double> m_path_btof2;
-			NTuple::Item<double> m_tof_btof2;
-			NTuple::Item<double> m_te_btof2;
-			NTuple::Item<double> m_tmu_btof2;
-			NTuple::Item<double> m_tpi_btof2;
-			NTuple::Item<double> m_tk_btof2;
-			NTuple::Item<double> m_tp_btof2;
-			NTuple::Item<double> m_ph_btof2;
-			NTuple::Item<double> m_zhit_btof2;
-			NTuple::Item<double> m_qual_btof2;
-	
-		// * With the method of momentum selection *
-		NTuple::Tuple* m_tuple8; 
-			NTuple::Item<double> m_mom_lepm;		    
-			NTuple::Item<double> m_mom_lepp;	    
-			NTuple::Item<double> m_mom_pionp;		    
-			NTuple::Item<double> m_mom_pionm;		    
-			NTuple::Item<double> m_pipi_dang;		    
-			NTuple::Item<double> m_cms_lepp;		    
-			NTuple::Item<double> m_cms_lepm;
-			NTuple::Item<double> m_mass_twopi;       
-			NTuple::Item<double> m_mass_jpsi;   
-			NTuple::Item<double> m_mass_recoil;   
-			NTuple::Item<double> m_inv_mass;   
-			NTuple::Item<double> m_tot_e;   
-			NTuple::Item<double> m_tot_px;   
-			NTuple::Item<double> m_tot_py;   
-			NTuple::Item<double> m_tot_pz;   
-			NTuple::Item<double> m_ep_ratio;
-			NTuple::Item<long>   m_event_flag;
-				// 3 or 4 tracks, 4=>4 tracks, 0=> miss pi+, 1=> miss pi-, 2=> miss lepton+, 3=> miss lepton-
-			NTuple::Item<double> m_trans_ratio_lepm;		    
-			NTuple::Item<double> m_trans_ratio_lepp;	    
-			NTuple::Item<double> m_trans_ratio_pionp;		    
-			NTuple::Item<double> m_trans_ratio_pionm;	    
-			NTuple::Item<long> m_run;
-			NTuple::Item<long> m_event;
-			NTuple::Item<long> m_index;
-			NTuple::Array<double> m_cos_theta;
-			NTuple::Array<double> m_phi;
-			NTuple::Matrix<double> m_four_mom;
-	
-		// * We don't need smear, actually, assume one track of pi pi is missing, the recoil mass is actually the invariant of pair-lepton *
-			NTuple::Item<long> m_pion_matched;
-			NTuple::Item<long> m_lep_matched;
+			// * Barrel outer ToF *
+				NTuple::Tuple* m_tuple6;
+				NTuple::Item<double> m_ptot_btof2;
+				NTuple::Item<double> m_cntr_btof2;
+				NTuple::Item<double> m_path_btof2;
+				NTuple::Item<double> m_tof_btof2;
+				NTuple::Item<double> m_te_btof2;
+				NTuple::Item<double> m_tmu_btof2;
+				NTuple::Item<double> m_tpi_btof2;
+				NTuple::Item<double> m_tk_btof2;
+				NTuple::Item<double> m_tp_btof2;
+				NTuple::Item<double> m_ph_btof2;
+				NTuple::Item<double> m_zhit_btof2;
+				NTuple::Item<double> m_qual_btof2;
 		
-		// * Book MCtruth *
-			NTuple::Item<long>   m_idxmc;
-			NTuple::Array<long>  m_pdgid;
-			NTuple::Array<long>  m_motheridx;
-			NTuple::Item<double> m_true_pionp;
-				// pions' momentum from MC truth   
-			NTuple::Item<double> m_true_pionm;   
+			// * With the method of momentum selection *
+				NTuple::Tuple* m_tuple8; 
+				NTuple::Item<double> m_mom_lepm;		    
+				NTuple::Item<double> m_mom_lepp;	    
+				NTuple::Item<double> m_mom_pionp;		    
+				NTuple::Item<double> m_mom_pionm;		    
+				NTuple::Item<double> m_pipi_dang;		    
+				NTuple::Item<double> m_cms_lepp;		    
+				NTuple::Item<double> m_cms_lepm;
+				NTuple::Item<double> m_mass_twopi;       
+				NTuple::Item<double> m_mass_jpsi;   
+				NTuple::Item<double> m_mass_recoil;   
+				NTuple::Item<double> m_inv_mass;   
+				NTuple::Item<double> m_tot_e;   
+				NTuple::Item<double> m_tot_px;   
+				NTuple::Item<double> m_tot_py;   
+				NTuple::Item<double> m_tot_pz;   
+				NTuple::Item<double> m_ep_ratio;
+				NTuple::Item<long>   m_event_flag;
+					// 3 or 4 tracks, 4=>4 tracks, 0=> miss pi+, 1=> miss pi-, 2=> miss lepton+, 3=> miss lepton-
+				NTuple::Item<double> m_trans_ratio_lepm;		    
+				NTuple::Item<double> m_trans_ratio_lepp;	    
+				NTuple::Item<double> m_trans_ratio_pionp;		    
+				NTuple::Item<double> m_trans_ratio_pionm;	    
+				NTuple::Item<long> m_run;
+				NTuple::Item<long> m_event;
+				NTuple::Item<long> m_index;
+				NTuple::Array<double> m_cos_theta;
+				NTuple::Array<double> m_phi;
+				NTuple::Matrix<double> m_four_mom;
+		
+			// * We don't need smear, actually, assume one track of pi pi is missing, the recoil mass is actually the invariant of pair-lepton *
+				NTuple::Item<long> m_pion_matched;
+				NTuple::Item<long> m_lep_matched;
+			
+			// * Book MCtruth *
+				NTuple::Item<long>   m_idxmc;
+				NTuple::Array<long>  m_pdgid;
+				NTuple::Array<long>  m_motheridx;
+				NTuple::Item<double> m_true_pionp;
+					// pions' momentum from MC truth   
+				NTuple::Item<double> m_true_pionm;   
 
-};
+			///@}
+
+	};
+
 
 /// @}
-
-
 #endif 
--- boss/workarea/Analysis/Physics/RhopiAlg/RhopiAlg-01-00-00/RhopiAlg/RhopiAlg.h
+++ boss/workarea/Analysis/Physics/RhopiAlg/RhopiAlg-01-00-00/RhopiAlg/RhopiAlg.h
@@ -1,189 +1,194 @@
 #ifndef Physics_Analysis_RhopiAlg_H
 #define Physics_Analysis_RhopiAlg_H
 
-/// @addtogroup BOSS_packages
-/// @{
-
-/**
- * @brief    Modified header of the stock BOSS package `RhopiAlg` (version `00-00-23`).
- * @author   Remco de Boer 雷穆克 (r.e.deboer@students.uu.nl or remco.de.boer@ihep.ac.cn)
- * @date     December 14th, 2018
- *
- * @details  This version of the `RhopiAlg` algorithm has more extensive comments. In addition, some additional parameters, such as ToF without particle hypothesis, have been added.
- */
-
-
-
 // * ========================= * //
 // * ------- LIBRARIES ------- * //
 // * ========================= * //
 	#include "GaudiKernel/AlgFactory.h"
 	#include "GaudiKernel/Algorithm.h"
 	#include "GaudiKernel/NTuple.h"
-	// #include "VertexFit/ReadBeamParFromDb.h"
 
 
 
 // * ================================ * //
 // * ------- CLASS DEFINITION ------- * //
 // * ================================ * //
-class RhopiAlg : public Algorithm
-{
-public:
-	// * Constructor and destructors *
-	RhopiAlg(const std::string& name, ISvcLocator* pSvcLocator);
+/// @addtogroup BOSS_packages
+/// @{
 
-	// * Algorithm steps *
-	StatusCode initialize();
-	StatusCode execute();
-	StatusCode finalize();
 
-private:
+	/**
+	 * @brief    Modified header of the stock BOSS package `RhopiAlg` (version `00-00-23`).
+	 * @author   Remco de Boer 雷穆克 (r.e.deboer@students.uu.nl or remco.de.boer@ihep.ac.cn)
+	 * @date     December 14th, 2018
+	 *
+	 * @details  This version of the `RhopiAlg` algorithm has more extensive comments. In addition, some additional parameters, such as ToF without particle hypothesis, have been added.
+	 */
+	class RhopiAlg : public Algorithm
+	{
+	public:
+		/// @name Constructors
+			///@{
+			RhopiAlg(const std::string& name, ISvcLocator* pSvcLocator);
+			///@}
 
-	// * Reader for beam info * //
-	// ReadBeamParFromDb fReader;
+		/// @name Information functions
+			///@{
+			StatusCode initialize();
+			StatusCode execute();
+			StatusCode finalize();
+			///@}
 
+	private:
+	// ! ================================= ! //
 	// ! ------- DECLARE CUTS HERE ------- ! //
-		// Here, you can define data members that you use to define cuts. The values for these cuts should be set in the `RhopiAlg::RhopiAlg` constructor (see `.cxx` file).
+	// ! ================================= ! //
+		/// @name Cut parameters
+		/// @details Here, you can define data members that you use to define cuts. The values for these cuts should be set in the `RhopiAlg::RhopiAlg` constructor (see `.cxx` file).
+			///@{
+			// * Declare r0, z0 cut for charged tracks
+				double fVr0cut;
+				double fVz0cut;
+				double fRvz0cut;
+				double fRvxy0cut;
 
-		// * Declare r0, z0 cut for charged tracks *
-		double fVr0cut;
-		double fVz0cut;
-		double fRvz0cut;
-		double fRvxy0cut;
+			// * Declare energy, dphi, dthe cuts for fake gamma's
+				double fEnergyThreshold;
+				double fGammaPhiCut;
+				double fGammaThetaCut;
+				double fGammaAngleCut;
 
-		// * Declare energy, dphi, dthe cuts for fake gamma's *
-		double fEnergyThreshold;
-		double fGammaPhiCut;
-		double fGammaThetaCut;
-		double fGammaAngleCut;
+			// * Declare invariant mass window cut
+				double fDeltaMrho0; // width of inv. mass window around mrho0
 
-		// * Declare invariant mass window cut *
-		double fDeltaMrho0; // width of inv. mass window around mrho0
+			// * Declare whether to test the success of the 4- and 5-constraint fits
+				bool fDo_fit4c;
+				bool fDo_fit5c;
+				double fMaxChiSq;
 
-		// * Declare whether to test the success of the 4- and 5-constraint fits *
-		bool fDo_fit4c;
-		bool fDo_fit5c;
-		double fMaxChiSq;
+			// * Declare whether or not to check success of Particle Identification
+				bool fCheckDedx;
+				bool fCheckTof;
 
-		// * Declare whether or not to check success of Particle Identification *
-		bool fCheckDedx;
-		bool fCheckTof;
+			///@}
 
 
+	// ! ==================================== ! //
 	// ! ------- DECLARE NTUPLES HERE ------- ! //
-		// NTuples are like vectors, but its members do not necessarily have to be of the same type. In this package, the NTuples are used to store event-by-event information. Its values are then written to the output ROOT file, creating a ROOT TTree. In that sense, each NTuple here represents one TTree within that output ROOT file, and each NTuple::Item represents its leaves. The name of the leaves is determined when calling NTuple::addItem.
-		// Note that the NTuple::Items are added to the NTuple during the RhopiAlg::initialize() step. This is also the place where you name these variables, so make sure that the structure here is reflected there!
+	// ! ==================================== ! //
+		/// @name `NTuple`s (eventual `TTree`s and their branches)
+		/// @details `NTuple`s are like vectors, but its members do not necessarily have to be of the same type. In this package, the NTuples are used to store event-by-event information. Its values are then written to the output ROOT file, creating a ROOT TTree. In that sense, each NTuple here represents one TTree within that output ROOT file, and each NTuple::Item represents its leaves. The name of the leaves is determined when calling `NTuple::addItem`. Note that the NTuple::Items are added to the NTuple during the `RhopiAlg::initialize()` step. This is also the place where you name these variables, so make sure that the structure here is reflected there!
+			///@{
+			// * Vertex information of the charged tracks
+				NTuple::Tuple* fTupleVxyz;
+				NTuple::Item<double> fVx0;
+				NTuple::Item<double> fVy0;
+				NTuple::Item<double> fVz0;
+				NTuple::Item<double> fVr0;
+				NTuple::Item<double> fRvxy0;
+				NTuple::Item<double> fRvz0;
+				NTuple::Item<double> fRvphi0;
 
-		// * Vertex information of the charged tracks *
-		NTuple::Tuple* fTupleVxyz;
-			NTuple::Item<double> fVx0;
-			NTuple::Item<double> fVy0;
-			NTuple::Item<double> fVz0;
-			NTuple::Item<double> fVr0;
-			NTuple::Item<double> fRvxy0;
-			NTuple::Item<double> fRvz0;
-			NTuple::Item<double> fRvphi0;
+			// * 'Fake' photon (angles)
+				NTuple::Tuple* fTupleAngles;
+				NTuple::Item<double> fDeltaTheta;
+				NTuple::Item<double> fDeltaPhi;
+				NTuple::Item<double> fDeltaAngle;
+				NTuple::Item<double> fEraw;
 
-		// * 'Fake' photon (angles) *
-		NTuple::Tuple* fTupleAngles;
-			NTuple::Item<double> fDeltaTheta;
-			NTuple::Item<double> fDeltaPhi;
-			NTuple::Item<double> fDeltaAngle;
-			NTuple::Item<double> fEraw;
+			// * Raw invariant mass of the two gammas and their total energy
+				NTuple::Tuple* fTupleMgg;
+				NTuple::Item<double> fMtoGG;
+				NTuple::Item<double> fEtot;
 
-		// * Raw invariant mass of the two gammas and their total energy *
-		NTuple::Tuple* fTupleMgg;
-			NTuple::Item<double> fMtoGG;
-			NTuple::Item<double> fEtot;
+			// * 4-constraint (4C) fit information
+				NTuple::Tuple* fTupleFit4C;
+				NTuple::Item<double> fChi1;
+				NTuple::Item<double> fMpi0;
 
-		// * 4-constraint (4C) fit information *
-		NTuple::Tuple* fTupleFit4C;
-			NTuple::Item<double> fChi1;
-			NTuple::Item<double> fMpi0;
+			// * 5-constraint (5C) fit information
+				NTuple::Tuple* fTupleFit5C;
+				NTuple::Item<double> fChi2;
+				NTuple::Item<double> fMrho0;
+				NTuple::Item<double> fMrhop;
+				NTuple::Item<double> fMrhom;
 
-		// * 5-constraint (5C) fit information *
-		NTuple::Tuple* fTupleFit5C;
-			NTuple::Item<double> fChi2;
-			NTuple::Item<double> fMrho0;
-			NTuple::Item<double> fMrhop;
-			NTuple::Item<double> fMrhom;
+			// * Photons
+				NTuple::Tuple* fTuplePhoton;
+				NTuple::Item<double> fFcos;
+				NTuple::Item<double> fElow;
 
-		// * Photons *
-		NTuple::Tuple* fTuplePhoton;
-			NTuple::Item<double> fFcos;
-			NTuple::Item<double> fElow;
+			// * Energy loss dE/dx
+				NTuple::Tuple* fTupleDedx;
+				NTuple::Item<double> fPtrack;
+				NTuple::Item<double> fChi2e;
+				NTuple::Item<double> fChi2mu;
+				NTuple::Item<double> fChi2pi;
+				NTuple::Item<double> fChi2k;
+				NTuple::Item<double> fChi2p;
+				NTuple::Item<double> fProbPH;
+				NTuple::Item<double> fNormPH;
+				NTuple::Item<double> fGhit;
+				NTuple::Item<double> fThit;
 
-		// * Energy loss dE/dx *
-		NTuple::Tuple* fTupleDedx;
-			NTuple::Item<double> fPtrack;
-			NTuple::Item<double> fChi2e;
-			NTuple::Item<double> fChi2mu;
-			NTuple::Item<double> fChi2pi;
-			NTuple::Item<double> fChi2k;
-			NTuple::Item<double> fChi2p;
-			NTuple::Item<double> fProbPH;
-			NTuple::Item<double> fNormPH;
-			NTuple::Item<double> fGhit;
-			NTuple::Item<double> fThit;
+			// * End cap ToF decector
+				NTuple::Tuple* fTupleTofEC;
+				NTuple::Item<double> fPtotTofEC;
+				NTuple::Item<double> fPathTofEC;
+				NTuple::Item<double> fTofEC;
+				NTuple::Item<double> fCntrTofEC;
+				NTuple::Item<double> fElectronTofEC;
+				NTuple::Item<double> fMuonTofEC;
+				NTuple::Item<double> fProtoniTofEC;
+				NTuple::Item<double> fKaonTofEC;
+				NTuple::Item<double> fProtonTofEC;
+				NTuple::Item<double> fPhTofEC;
+				NTuple::Item<double> fRhitTofEC;
+				NTuple::Item<double> fQualTofEC;
 
-		// * End cap ToF decector *
-		NTuple::Tuple* fTupleTofEC;
-			NTuple::Item<double> fPtotTofEC;
-			NTuple::Item<double> fPathTofEC;
-			NTuple::Item<double> fTofEC;
-			NTuple::Item<double> fCntrTofEC;
-			NTuple::Item<double> fElectronTofEC;
-			NTuple::Item<double> fMuonTofEC;
-			NTuple::Item<double> fProtoniTofEC;
-			NTuple::Item<double> fKaonTofEC;
-			NTuple::Item<double> fProtonTofEC;
-			NTuple::Item<double> fPhTofEC;
-			NTuple::Item<double> fRhitTofEC;
-			NTuple::Item<double> fQualTofEC;
+			// * Inner barrel ToF decector
+				NTuple::Tuple* TupleTofIB;
+				NTuple::Item<double> fPtotTofIB;
+				NTuple::Item<double> fPathTofIB;
+				NTuple::Item<double> fTofIB;
+				NTuple::Item<double> fCntrTofIB;
+				NTuple::Item<double> fElectronTofIB;
+				NTuple::Item<double> fMuonTofIB;
+				NTuple::Item<double> fProtoniTofIB;
+				NTuple::Item<double> fKaonTofIB;
+				NTuple::Item<double> fProtonTofIB;
+				NTuple::Item<double> fPhTofIB;
+				NTuple::Item<double> fZhitTofIB;
+				NTuple::Item<double> fQualTofIB;
 
-		// * Inner barrel ToF decector *
-		NTuple::Tuple* TupleTofIB;
-			NTuple::Item<double> fPtotTofIB;
-			NTuple::Item<double> fPathTofIB;
-			NTuple::Item<double> fTofIB;
-			NTuple::Item<double> fCntrTofIB;
-			NTuple::Item<double> fElectronTofIB;
-			NTuple::Item<double> fMuonTofIB;
-			NTuple::Item<double> fProtoniTofIB;
-			NTuple::Item<double> fKaonTofIB;
-			NTuple::Item<double> fProtonTofIB;
-			NTuple::Item<double> fPhTofIB;
-			NTuple::Item<double> fZhitTofIB;
-			NTuple::Item<double> fQualTofIB;
+			// * Outer barrel ToF decector
+				NTuple::Tuple* TupleTofOB;
+				NTuple::Item<double> fPtotTofOB;
+				NTuple::Item<double> fPathTofOB;
+				NTuple::Item<double> fTofOB;
+				NTuple::Item<double> fCntrTofOB;
+				NTuple::Item<double> fElectronTofOB;
+				NTuple::Item<double> fMuonTofOB;
+				NTuple::Item<double> fProtoniTofOB;
+				NTuple::Item<double> fKaonTofOB;
+				NTuple::Item<double> fProtonTofOB;
+				NTuple::Item<double> fPhTofOB;
+				NTuple::Item<double> fZhitTofOB;
+				NTuple::Item<double> fQualTofOB;
 
-		// * Outer barrel ToF decector *
-		NTuple::Tuple* TupleTofOB;
-			NTuple::Item<double> fPtotTofOB;
-			NTuple::Item<double> fPathTofOB;
-			NTuple::Item<double> fTofOB;
-			NTuple::Item<double> fCntrTofOB;
-			NTuple::Item<double> fElectronTofOB;
-			NTuple::Item<double> fMuonTofOB;
-			NTuple::Item<double> fProtoniTofOB;
-			NTuple::Item<double> fKaonTofOB;
-			NTuple::Item<double> fProtonTofOB;
-			NTuple::Item<double> fPhTofOB;
-			NTuple::Item<double> fZhitTofOB;
-			NTuple::Item<double> fQualTofOB;
+			// * Particle ID info
+				NTuple::Tuple* fTuplePID;
+				NTuple::Item<double> fPtrackPID;
+				NTuple::Item<double> fCostPID;
+				NTuple::Item<double> fDedxPID;
+				NTuple::Item<double> fTof1PID;
+				NTuple::Item<double> fTof2PID;
+				NTuple::Item<double> fProbPID;
 
-		// * Particle ID info *
-		NTuple::Tuple* fTuplePID;
-			NTuple::Item<double> fPtrackPID;
-			NTuple::Item<double> fCostPID;
-			NTuple::Item<double> fDedxPID;
-			NTuple::Item<double> fTof1PID;
-			NTuple::Item<double> fTof2PID;
-			NTuple::Item<double> fProbPID;
+			///@}
 
-};
+	};
+
 
 /// @}
-
-
 #endif
